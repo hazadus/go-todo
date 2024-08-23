5e19ccfb1e5c5ab755b5ec0366c4a9d268d04180
hazadus <hazadus7@gmail.com>
2024-08-23 11:09:11 +0300

    tests: Implement AddNewTaskFromStdin test.

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -43,6 +44,7 @@ func TestMain(m *testing.M) {
 
 func TestTodoCLI(t *testing.T) {
 	task := "Test task 1"
+	task2 := "Test task 2"
 
 	dir, err := os.Getwd()
 	if err != nil {
@@ -59,6 +61,21 @@ func TestTodoCLI(t *testing.T) {
 		}
 	})
 
+	t.Run("AddNewTaskFromStdin", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+
+		cmdStdIn, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdIn, task2)
+		cmdStdIn.Close()
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
 	t.Run("ListTasks", func(t *testing.T) {
 		cmd := exec.Command(cmdPath, "-list")
 
@@ -67,7 +84,7 @@ func TestTodoCLI(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		expected := fmt.Sprintf("[ ] 1: %s\n", task)
+		expected := fmt.Sprintf("[ ] 1: %s\n[ ] 2: %s\n", task, task2)
 		if expected != string(output) {
 			t.Errorf("Expected %q, got %q instead\n", expected, string(output))
 		}
